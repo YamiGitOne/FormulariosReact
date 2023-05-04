1ebc3992389f3d51ad8c6d4ec3bc293c902cc755
Yamilet Jerez <quieromiwww@gmail.com>
2023-05-04 21:37:18 +0200

    -

--- src/formularios/NoControlado.jsx
+++ src/formularios/NoControlado.jsx
@@ -1,16 +1,30 @@
+import { useRef } from "react";
+
 const NoControlado = () => {
+    const form = useRef(null);
+    //const [error, setError] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("me diste click");
+        
+        const data = new FormData(form.current);
+        console.log([...data.entries()]);
+
+        //validar datos
+       // if (!titleReceta.trim()) return setError('Llena todos los campos');
+
+        //enviar datos
+        const { titleReceta, descripcionReceta, state } = Object.fromEntries([...data.entries()]);
+  
     };
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} ref={form}>
             <input 
             type="text" placeholder="Ingresa la receta" 
             className="form-control mb-2" 
             name="titleReceta" 
+            defaultValue="titleReceta1"
             />
 
             <textarea 
@@ -22,11 +36,12 @@ const NoControlado = () => {
             <select 
             className="form-select mb-2" 
             name="state">
-                <option value="opcion 1">opcion 1</option>
-                <option value="opcion 2">opcion 2</option>
+                <option value="opcion 1">opción 1</option>
+                <option value="opcion 2">opción 2</option>
             </select>
 
             <button type="submit" className="btn btn-primary">Procesar</button>
+          
         </form>
         
     );
