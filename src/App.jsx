d1ab46cff2c482295b3a181f42f53e291eb6efd8
Yamilet Jerez <quieromiwww@gmail.com>
2023-06-05 14:12:09 +0200

    useEffect + LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,7 @@ import {useState} from "react";
 import Formulario from "./formularios/Formulario";
 import Todos from "./formularios/Todos"
 
-const inicialStateTodos = [
-  {
-    id:1,
-    title: 'Todo #01',
-    description: 'Descripción #01',
-    state: true,
-    priority: true
-  },
-  {
-    id:2,
-    title: 'Todo #02',
-    description: 'Descripción #02',
-    state: false,
-    priority: false
-  },
-  {
-    id:3,
-    title: 'Todo #03',
-    description: 'Descripción #03',
-    state: false,
-    priority: true
-  },
-]
+const inicialStateTodos = [];
 
 const App = () => {
 
